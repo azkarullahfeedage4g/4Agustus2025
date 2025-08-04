1180c2ea3fa17e13d631eaae761974866623244b
azkar <rakzaff@gmail.com>
2025-08-04 14:34:48 +0700

    Add files via upload

---
+++ IsiNamaFile2.java
@@ -0,0 +1,19 @@
+public class IsiNamaFile2 {
+public static void main(String[] args) {
+    //main method
+    System.out.println("Azkarullah");
+    System.out.println("SMK Telkom Malang");
+    System.out.println("SMP Negeri 2 Lumajang");
+    System.out.println("ARJUN");
+    System.out.println("ENDAH HASTA");
+    System.out.println("Web Dev & Actor");
+    System.out.println("Basket");
+
+    //variabel -> tipe_data nama_variabel = value;
+    String nama = "Azka Rullah";
+    int usia = 16;
+    System.out.println(nama);
+    System.out.println(usia + " tahun");
+    
+}
+}
---
+++ namatemancitacitausia.java
@@ -0,0 +1,46 @@
+public class namatemancitacitausia {
+public static void main(String[] args) {
+    //main method
+
+String nama = "Nabil Aryaguna";
+String usia = "16 Tahun";
+String citacita = "Programmer";
+
+String NamaA = "Ghaisan Afif";
+int UsiaA = 16;
+String CitacitaA = "Software Engineer";
+
+String NameB = "Faliha Arfa";
+int AgeB = 15;
+String CitaCitaB = "Traveller";
+
+String NAMAC = "Azka Azzam Mubarrok";
+int USIAC = 15;
+String CitacitaB = "Programmer";
+
+String NAMAD = "Farhan Haidas";
+int USIAD = 15;
+String CitacitaD = "Pengusaha";
+
+    System.out.println(nama);
+    System.out.println(usia);
+    System.out.println(citacita);
+
+    System.out.println(NamaA);
+    System.out.println(UsiaA);
+    System.out.println(CitacitaA);
+
+    System.out.println(NameB);
+    System.out.println(AgeB);
+    System.out.println(CitaCitaB);
+
+    System.out.println(NAMAC);
+    System.out.println(USIAC);
+    System.out.println(CitacitaB);
+
+    System.out.println(NAMAD);
+    System.out.println(USIAD);
+    System.out.println(CitacitaD);
+
+}
+}
